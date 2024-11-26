14d8eab03cec24670ebc0aed53503783c1b09dc0
Pingugou <ramirogernotmonteroperez2@gmail.com>
2024-11-26 12:14:10 -0400

    java

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,28 @@
-let mensaje= "hola mundo";
+let mensaje= "hola jer";
 
 function sumar(){
-let numero1=3;
-let numero2=4;
+let numero1=4;
+let numero2=5;
 let suma=numero1+numero2
-alert("la suma es"+suma)
+alert("La suma es"+suma)
 }
-
-
 function restar(){
-    let numero1=3;
-    let numero2=4;
-    let restar=numero1-numero2
-    alert("la resta es"+restar)
+    let numero1=4;
+    let numero2=5;
+    let resta=numero1-numero2
+    alert("La resta es"+resta)
     }
 
-    
-function multiplicar(){
-    let numero1=3;
-    let numero2=4;
-    let multiplicar=numero1*numero2
-    alert("la multiplicacion es"+multiplicar)
+    function multiplicacion(){
+        let numero1=4;
+        let numero2=5;
+        let multiplicacion=numero1*numero2
+        alert("La multiplicacion es"+multiplicacion)
     }
 
-    
-function dividir(){
-    let numero1=3;
-    let numero2=4;
-    let dividir=numero1/numero2
-    alert("la divicion es"+dividir)
+    function division(){
+        let numero1=4;
+        let numero2=5;
+        let division=numero1/numero2
+        alert("La division es"+division)
     }
-
-    
